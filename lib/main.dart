9a939a0dc1701b37847193376b021c31f9c442c3
Ryuga-Y <zhengyao1128@gmail.com>
2025-06-03 18:44:47 +0800

    HI

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Study Hub',
+      title: 'Study Hub HII',
       home: SignUpPage(),
     );
   }
